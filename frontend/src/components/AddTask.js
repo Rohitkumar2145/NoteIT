55f0d13356ab3cea91bd75e8832e9b13e3ca4fc9
RohitKumar Sharma <rohitsharmars2106@gmail.com>
2023-08-29 11:39:59 +0530

    Updating AddTask.js

--- frontend/src/components/AddTask.js
+++ frontend/src/components/AddTask.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import React from "react";
 import AddTaskButton from "./TaskModal/AddTaskButton";
 import TaskForm from "./TaskModal/TaskForm";
